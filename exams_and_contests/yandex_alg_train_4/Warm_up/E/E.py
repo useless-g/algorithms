a807f31b5c28262b19051ef0a298fbbc45bc55bf
Sergey Bychenkov <uzellessmovic@gmail.com>
2023-11-05 02:19:39 +0300

    asda

--- exams_and_contests/yandex_alg_train_4/Warm_up/E/E.py
+++ exams_and_contests/yandex_alg_train_4/Warm_up/E/E.py
@@ -1,30 +1,17 @@
-from collections import deque
-
 n = int(input())
-students = deque(map(int, input().split(" ")))
+students = list(map(int, input().split(" ")))
 
-hate = []
+hate_left = [0 for _ in range(n)]
+hate_right = [0 for _ in range(n)]
 
-for i in range(n):
-    new_deque = deque()
-    student = students.popleft()
-    for j in range(n-i-1):
-        new_deque.append(abs(student - students[j]))
-    s = sum(new_deque)
-    for j in range(i):
-        s += hate[j].popleft()
-    print(s, end=" ")
-    hate.append(new_deque)
+s = 0
+for i in range(1, n):
+    s += students[i-1]
+    hate_left[i] = students[i] * i - s
 
+s = 0
+for i in reversed(range(n - 1)):
+    s += students[i + 1]
+    hate_right[i] = s - students[i] * (n-1-i)
 
-
-n = int(input())
-a = list(map(int, input().split()))
-
-prefix_sum = [0 for _ in range(n + 1)]
-for i in range(1, n + 1):
-    prefix_sum[i] = prefix_sum[i - 1] + a[i - 1]
-
-for i in range(n):
-    dissatisfaction = (i * a[i] - prefix_sum[i]) + ((prefix_sum[n] - prefix_sum[i + 1]) - (n - i - 1) * a[i])
-    print(dissatisfaction, end=' ')
+print(" ".join([str(hate_left[i] + hate_right[i]) for i in range(n)]))
