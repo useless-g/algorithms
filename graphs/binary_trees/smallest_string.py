7e3dea85f9672c75c30b528ec91115eb266a1eca
Bychenkov Sergey <s.bychenkov@stc-spb.ru>
2024-04-17 15:56:03 +0300

    smallest string from leaf to root in bintree

--- graphs/binary_trees/smallest_string.py
+++ graphs/binary_trees/smallest_string.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/smallest-string-starting-from-leaf
+# top 100% mem!!!!!!!!!
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,17 +8,14 @@
 #         self.right = right
 class Solution(object):
     def go_deep(self, root, depth):
-        if (depth > self.min_depth) or (root is None):
+        if root is None:
             return
 
         self.candidate.append(root.val)
-        print(self.candidate, self.candidates, self.min_depth)
+        # print(self.candidate, self.candidates, self.min_depth)
         if (root.left is None) and (root.right is None):
-            if depth == self.min_depth:
-                self.candidates.append(self.candidate[:])
-            else:  # depth < self.min_depth
-                self.candidates = [self.candidate[:]]
-                self.min_depth = depth
+            # self.candidates.append(self.candidate[:])
+            self.result = min(self.result, self.candidate[::-1])
         else:
             self.go_deep(root.left, depth + 1)
             self.go_deep(root.right, depth + 1)
@@ -28,19 +26,8 @@ class Solution(object):
         :type root: TreeNode
         :rtype: str
         """
-        # иметь на каждой итерации минимальную длину
-        # глубину захода тоже надо иметь
-        # как только глубина захода становится больше минимальной длины мы выходим
-        # если длина оказалась меньше чем минимальная то затираем список кандидатов, обновляем минимальную длину
-        # когда пройдем все дерево, нужно отсортировать кандидатов и вернуть [0] элемент
-        # самое главное:
-        # мы должны хранить всю строку до момента пока не упремся в (root.left is None) or (root.right is None)
-        # тогда у нас новый кандидат, если длина равна минимальной
-        # если не равна то должна быть меньше, то есть обновляем self.min_depth = depth и self.candidates = [root]
-
-        self.min_depth = 8501
-        self.candidates = []
+        self.result = [26]
         self.candidate = []
         self.go_deep(root, 1)
-        return "".join(list(map(lambda x: chr(x + 97), sorted(list(map(lambda x: x[::-1], self.candidates)))[0])))
+        return "".join(list(map(lambda x: chr(x + 97), self.result)))
 
