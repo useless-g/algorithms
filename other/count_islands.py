0f173a2574e97d00a466ab8d8d35fcfe1b0bcd2f
Bychenkov Sergey <s.bychenkov@stc-spb.ru>
2024-04-19 14:44:36 +0300

    count_islands

---
+++ other/count_islands.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/number-of-islands/
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # ищем первую землю
+        # дальше идем поиском в ширину и ставим в вершину island_number
+        # когда закончили ище дальше непомеченную землю и повторяем
+        # возвращаем island_number
+        island_number = 0
+        row = len(grid)
+        col = len(grid[0])
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j] == "1":  # unmarked ground
+                    neighbors = [(i, j)]
+                    while neighbors:
+                        # print(neighbors)
+                        ii, jj = neighbors.pop()
+                        grid[ii][jj] = island_number
+
+                        if (ii != 0) and (grid[ii - 1][jj] == "1"):  # unmarked ground up
+                            neighbors.append((ii - 1, jj))
+                        if (ii != row - 1) and (grid[ii + 1][jj] == "1"):  # unmarked ground down
+                            neighbors.append((ii + 1, jj))
+                        if (jj != col - 1) and (grid[ii][jj + 1] == "1"):  # unmarked ground right
+                            neighbors.append((ii, jj + 1))
+                        if (jj != 0) and (grid[ii][jj - 1] == "1"):  # unmarked ground left
+                            neighbors.append((ii, jj - 1))
+                    island_number += 1
+        return island_number
+
+
+
