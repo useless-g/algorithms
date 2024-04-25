bc8f869cd8720da80549ac08adc84dfd50dcd6b6
Bychenkov Sergey <s.bychenkov@stc-spb.ru>
2024-04-25 17:59:30 +0300

    yep fast works dp str

--- DP/longest_ideal_subsequence.py
+++ DP/longest_ideal_subsequence.py
@@ -1,17 +1,16 @@
 # https://leetcode.com/problems/longest-ideal-subsequence
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-        s = tuple(map(ord, list(s)))
-        dp = [0] * len(s)
-        dp[0] = 1
-        for i in range(1, len(s)):
-            j = i - 1
-            while j >= 0:
-                if abs(s[i] - s[j]) <= k:
-                    dp[i] = max(dp[i], dp[j] + 1)
-                j -= 1
-            dp[i] = dp[i] if dp[i] else 1
+        # s = tuple(map(lambda x: ord(x) - 97, list(s)))
+        dp = [0] * 26
+        for i in s:
+            m = 0
+            i = ord(i) - 97
+            for j in range(i-k, i+k+1):
+                if 0 <= j <= 25:
+                    m = max(m, dp[j])
+            dp[i] = m + 1
 
         return max(dp)
 
-print(Solution().longestIdealString("eduktdb", 15))
+print(Solution().longestIdealString("acfgbd", 2))
